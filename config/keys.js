9501432915d08f83b6cf3858c4da93117e980f5a
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-04-13 13:19:11 +0530

    key sucks!!!

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
   mongoURI: 'mongodb://Harshitb1:66454477.h@ds139576.mlab.com:39576/hackdelhiapi',
-
   secretOrKey: 'secret'
 };
