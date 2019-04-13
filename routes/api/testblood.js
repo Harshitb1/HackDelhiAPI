64849fa02813df7697818467ac449a52ecee4a0d
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-04-13 19:25:22 +0530

    blood error

--- routes/api/testblood.js
+++ routes/api/testblood.js
@@ -11,43 +11,40 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
 // Load User model
-const Blood = require('../../models/Blood');
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-// router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
-
-router.get('/test', (req, res) => res.json({ msg: 'blood Works' }));
+router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
 // @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
-//   const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
   // Check Validation
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-//   User.findOne({ email: req.body.email }).then(user => {
-//     if (user) {
-//       errors.email = 'Email already exists';
-//       return res.status(400).json(errors);
-//     } else {
-//       const avatar = gravatar.url(req.body.email, {
-//         s: '200', // Size
-//         r: 'pg', // Rating
-//         d: 'mm' // Default
-//       });
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
 
-      const newBlood = new Blood({
-         bloodgroup: req.body.bloodgroup,
-         location: req.body.location,
-         user: req.body.user
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email
       }).save()
-        .then(blood => res.json(blood))
+        .then(user => res.json(user))
         .catch(err => console.log(err));
       // bcrypt.genSalt(10, (err, salt) => {
       //   bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -59,20 +56,8 @@ router.post('/register', (req, res) => {
       //       .catch(err => console.log(err));
       //   });
       // });
-    // }
-//   });
-});
-
-
-router.get("/:blood_id",function(req,res){
-    Blood.findById(req.params.blood_id,function(err,blood){
-        if(err){
-            console.log(err);
-           return res.status(404).json(err);
-        } else {
-           res.json(blood);
-       }
-    });
+    }
+  });
 });
 
 // @route   GET api/users/login
@@ -127,7 +112,7 @@ router.post('/login', (req, res) => {
 // @desc    Return current user
 // @access  Private
 router.get(
-  '/:id',
+  '/current',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     res.json({
